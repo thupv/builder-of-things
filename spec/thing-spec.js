70bf42a3a12e16eae847f710d7f190af0010a2ba
Thu Pham <thupv.personal@gmail.com>
2018-07-16 13:30:09 +0700

    Add define property, define child, define method

--- spec/thing-spec.js
+++ spec/thing-spec.js
@@ -26,9 +26,48 @@ describe('Thing', () => {
   describe('properties per instance level', () => {
     const jane = new Thing('jane');
     it('should property work', () => {
-      jane.is_the.parent_of.joe
+      jane.is_the.parent_of.joe;
       expect(jane.parent_of).toBe('joe');
     });
   });
 
+  describe('define number of childs things', () => {
+    const jane = new Thing('jane');
+    it('multiple items', () => {
+      jane.has(2).legs;
+      expect(jane.legs.length).toBe(2);
+      expect(jane.legs[0] instanceof Thing).toBe(true);
+    });
+    it('single items', () => {
+      jane.has(1).head;
+      expect(jane.head instanceof Thing).toBe(true);
+    });
+    it('chainable', () => {
+      jane.has(2).arms.forEach(arm => arm.having(1).hand.having(5).fingers);
+      expect(jane.arms[0].hand.fingers.length).toBe(5);
+    });
+  });
+
+  describe('define nested property', () => {
+    const jane = new Thing('jane');
+    it('being_the', () => {
+      jane.has(1).head.having(2).eyes.forEach(eye => eye.being_the.color.green);
+      expect(jane.head.eyes[0].color).toBe('green');
+    });
+    it('and_the', () => {
+      jane.has(1).head.having(2).eyes.forEach(eye => eye.being_the.color.blue.and_the.shape.round);
+      expect(jane.head.eyes[0].color).toBe('blue');
+      expect(jane.head.eyes[0].shape).toBe('round');
+    });
+  });
+
+  describe('define method', () => {
+    const jane = new Thing('jane');
+    it('should work', () => {
+      jane.can.speak('spoke', phrase => `${name} says: ${phrase}!`);
+      jane.speak('hi');
+      expect(jane.spoke).toBe('jane says: hi!');
+    });
+
+  });
 });
--- src/thing.js
+++ src/thing.js
@@ -1,7 +1,9 @@
 const PROXY_MODE = {
   NONE: 0,
   DEFINE_BOOLEAN_MODE: 1,
-  DEFINE_PROPERTY_MODE: 2
+  DEFINE_PROPERTY_MODE: 2,
+  DEFINE_CHILD_THING: 3,
+  DEFINE_METHOD: 4
 };
 
 export class Thing {
@@ -25,6 +27,28 @@ function resetProxy(target) {
 function proxify(instance) {
   return new Proxy(instance, {
     get: (target, prop, receiver) => {
+      if (target._proxy.mode === PROXY_MODE.DEFINE_METHOD) {
+        resetProxy(target);
+        return (fnName, fn) => {
+          target[prop] = (...args) => {
+            const res = instance_call(target, fn, ...args);
+            target[fnName] = res;
+          };
+        }
+      }
+
+      if (target._proxy.mode === PROXY_MODE.DEFINE_CHILD_THING) {
+        if (target._proxy.value === 1) {
+          target[prop] = new Thing();
+        } else if (target._proxy.value > 1) {
+          target[prop] = [];
+          for (let i = 0; i < target._proxy.value; i++) {
+            target[prop].push(new Thing());
+          }
+        }
+        resetProxy(target);
+      }
+
       if (target._proxy.mode === PROXY_MODE.DEFINE_BOOLEAN_MODE) {
         target[prop] = target._proxy.value;
         resetProxy(target);
@@ -34,7 +58,7 @@ function proxify(instance) {
         if (target._proxy.value) {
           target[target._proxy.value] = prop;
           resetProxy(target);
-          return prop;
+          return receiver;
         } else {
           target._proxy.value = prop;
           return receiver;
@@ -49,9 +73,22 @@ function proxify(instance) {
         return receiver;
       }
 
-      if (prop === 'is_the') {
+      if (prop === 'is_the' || prop === 'being_the' || prop === 'and_the') {
         target._proxy.mode = PROXY_MODE.DEFINE_PROPERTY_MODE;
         target._proxy.value = null;
+        return receiver;
+      }
+
+      if (prop === 'has' || prop === 'having') {
+        return (numberOfThings) => {
+          target._proxy.mode = PROXY_MODE.DEFINE_CHILD_THING;
+          target._proxy.value = numberOfThings;
+          return receiver;
+        };
+      }
+
+      if (prop === 'can') {
+        target._proxy.mode = PROXY_MODE.DEFINE_METHOD;
         return receiver;
       }
 
@@ -66,4 +103,17 @@ function proxify(instance) {
       return target[prop];
     }
   })
+}
+
+function instance_call(instance, fn, ...args) {
+  const oldWindow = window;
+  for(let attr in instance) {
+    window[attr] = instance[attr];
+  }
+  const res = fn(args);
+
+  for(let attr in instance) {
+    window[attr] = oldWindow[attr];
+  }
+  return res;
 }
